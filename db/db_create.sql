d0d2fd343d70e1c5e4c21fa8820177a2a530e937
Pekka Pekkonen <pekka.pekkonen@live.fi>
2016-05-28 22:59:42 +0200

    add database script that is used to set up environment

---
+++ db/db_create.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:8889
+-- Generation Time: May 28, 2016 at 10:47 PM
+-- Server version: 5.5.42
+-- PHP Version: 7.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `mobileweb-amazeme`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE `question` (
+  `word_id` int(11) NOT NULL,
+  `picture_link` varchar(100) COLLATE utf8_swedish_ci NOT NULL,
+  `english` varchar(60) COLLATE utf8_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `time_between_images` int(11) NOT NULL DEFAULT '5',
+  `play_audio_example` bit(1) NOT NULL DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `login_name` varchar(40) COLLATE utf8_swedish_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `tries` int(11) NOT NULL DEFAULT '0',
+  `tries_right` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`word_id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `word_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `settings`
+--
+ALTER TABLE `settings`
+  ADD CONSTRAINT `Id relation` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+INSERT INTO `question` (`word_id`, `picture_link`, `english`) VALUES
+(1, './assets/images/bicycle.jpg', 'bicycle'),
+(2, './assets/images/car.png', 'car'),
+(3, './assets/images/apple-logo.png', 'apple');
