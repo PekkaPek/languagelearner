7fbca111dba5ff09901cc7837ec9aedd3a0318ac
Pekka Pekkonen <pekka.pekkonen@live.fi>
2016-05-29 10:52:27 +0200

    change SQL dump to create the database as well if not existing

--- db/db_create.sql
+++ db/db_create.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: May 28, 2016 at 10:47 PM
+-- Generation Time: May 29, 2016 at 10:40 AM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.0
 
@@ -13,6 +13,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `mobileweb-amazeme`
 --
+CREATE DATABASE IF NOT EXISTS `mobileweb-amazeme` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `mobileweb-amazeme`;
 
 -- --------------------------------------------------------
 
@@ -25,6 +27,15 @@ CREATE TABLE `question` (
   `picture_link` varchar(100) COLLATE utf8_swedish_ci NOT NULL,
   `english` varchar(60) COLLATE utf8_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`word_id`, `picture_link`, `english`) VALUES
+  (1, './assets/images/bicycle.jpg', 'bicycle'),
+  (2, './assets/images/car.png', 'car'),
+  (3, './assets/images/apple-logo.png', 'apple');
 
 -- --------------------------------------------------------
 
@@ -60,20 +71,20 @@ CREATE TABLE `user` (
 -- Indexes for table `question`
 --
 ALTER TABLE `question`
-  ADD PRIMARY KEY (`word_id`);
+ADD PRIMARY KEY (`word_id`);
 
 --
 -- Indexes for table `settings`
 --
 ALTER TABLE `settings`
-  ADD PRIMARY KEY (`user_id`),
-  ADD KEY `user_id` (`user_id`);
+ADD PRIMARY KEY (`user_id`),
+ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -83,12 +94,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `word_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `word_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -97,9 +108,4 @@ ALTER TABLE `user`
 -- Constraints for table `settings`
 --
 ALTER TABLE `settings`
-  ADD CONSTRAINT `Id relation` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-INSERT INTO `question` (`word_id`, `picture_link`, `english`) VALUES
-(1, './assets/images/bicycle.jpg', 'bicycle'),
-(2, './assets/images/car.png', 'car'),
-(3, './assets/images/apple-logo.png', 'apple');
+ADD CONSTRAINT `Id relation` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
